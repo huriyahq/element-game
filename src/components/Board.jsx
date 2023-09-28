5ca3c70a51e38682b5e7a8fc3d6e336c7db3e9fe
Huriyah Quadri <huriyah.quadri@fdmgroup.com>
2023-09-28 14:13:17 +0100

    Highlight player selection

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Board.css";
 import Element from "./Element";
 
 const Board = () => {
+	const [selectedElement, setSelectedElement] = useState(null);
+
+	const handleElementClick = (elementType) => {
+		setSelectedElement(elementType);
+	};
 	return (
 		<div className="board">
 			<Element
 				type="fire"
 				className="element-1"
+				isSelected={selectedElement === "fire"}
+				onClick={() => handleElementClick("fire")}
 			/>
 			<Element
 				type="water"
 				className="element-2"
+				isSelected={selectedElement === "water"}
+				onClick={() => handleElementClick("water")}
 			/>
 			<Element
 				type="earth"
 				className="element-3"
+				isSelected={selectedElement === "earth"}
+				onClick={() => handleElementClick("earth")}
 			/>
 			<Element
 				type="air"
 				className="element-4"
+				isSelected={selectedElement === "air"}
+				onClick={() => handleElementClick("air")}
 			/>
 		</div>
 	);
--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -2,47 +2,39 @@ import React from "react";
 import "../styles/Element.css";
 import { FireIcon, WaterIcon, EarthIcon, AirIcon } from "../utils/icons";
 
-const Element = ({ type }) => {
+const Element = ({ type, isSelected, onClick }) => {
 	const lowerCaseType = type.toLowerCase();
 	let icon;
 
 	switch (lowerCaseType) {
 		case "fire":
-			icon = (
-				<FireIcon
-					className="element"
-					// sx={{ fontSize: 80 }}
-				/>
-			);
+			icon = <FireIcon className={`element ${isSelected ? "selected" : ""}`} />;
 			break;
 		case "water":
 			icon = (
-				<WaterIcon
-					className="element"
-					// sx={{ fontSize: 80 }}
-				/>
+				<WaterIcon className={`element ${isSelected ? "selected" : ""}`} />
 			);
 			break;
 		case "earth":
 			icon = (
-				<EarthIcon
-					className="element"
-					// sx={{ fontSize: 80 }}
-				/>
+				<EarthIcon className={`element ${isSelected ? "selected" : ""}`} />
 			);
 			break;
 		case "air":
-			icon = (
-				<AirIcon
-					className="element"
-					// sx={{ fontSize: 80 }}
-				/>
-			);
+			icon = <AirIcon className={`element ${isSelected ? "selected" : ""}`} />;
 			break;
 		default:
 			icon = null;
 	}
-	return <div className="element-box">{icon}</div>;
+
+	return (
+		<div
+			className="element-box"
+			onClick={onClick}
+		>
+			{icon}
+		</div>
+	);
 };
 
 export default Element;
