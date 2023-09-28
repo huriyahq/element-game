fa4b6df1efeef2863fe1b3246285fbf6b3fd114e
Huriyah Quadri <huriyah.quadri@fdmgroup.com>
2023-09-28 14:48:18 +0100

    Display player and computer selections

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,37 +3,65 @@ import "../styles/Board.css";
 import Element from "./Element";
 
 const Board = () => {
-	const [selectedElement, setSelectedElement] = useState(null);
+	const [playerSelectedElement, setPlayerSelectedElement] = useState(null);
+	const [computerSelectedElement, setComputerSelectedElement] = useState(null);
 
 	const handleElementClick = (elementType) => {
-		setSelectedElement(elementType);
+		setPlayerSelectedElement(elementType);
+		selectComputerElement();
 	};
+
+	const selectComputerElement = () => {
+		const elements = ["fire", "water", "earth", "air"];
+		const index = Math.floor(Math.random() * elements.length);
+		const element = elements[index];
+		setComputerSelectedElement(element);
+	};
+
 	return (
 		<div className="board">
-			<Element
-				type="fire"
-				className="element-1"
-				isSelected={selectedElement === "fire"}
-				onClick={() => handleElementClick("fire")}
-			/>
-			<Element
-				type="water"
-				className="element-2"
-				isSelected={selectedElement === "water"}
-				onClick={() => handleElementClick("water")}
-			/>
-			<Element
-				type="earth"
-				className="element-3"
-				isSelected={selectedElement === "earth"}
-				onClick={() => handleElementClick("earth")}
-			/>
-			<Element
-				type="air"
-				className="element-4"
-				isSelected={selectedElement === "air"}
-				onClick={() => handleElementClick("air")}
-			/>
+			<div className="options">
+				<Element
+					type="fire"
+					className="element-1"
+					isSelected={playerSelectedElement === "fire"}
+					onClick={() => handleElementClick("fire")}
+				/>
+				<Element
+					type="water"
+					className="element-2"
+					isSelected={playerSelectedElement === "water"}
+					onClick={() => handleElementClick("water")}
+				/>
+				<Element
+					type="earth"
+					className="element-3"
+					isSelected={playerSelectedElement === "earth"}
+					onClick={() => handleElementClick("earth")}
+				/>
+				<Element
+					type="air"
+					className="element-4"
+					isSelected={playerSelectedElement === "air"}
+					onClick={() => handleElementClick("air")}
+				/>
+			</div>
+			<div className="selections">
+				{playerSelectedElement && (
+					<Element
+						type={playerSelectedElement}
+						className="element-player"
+						isSelected={true}
+					/>
+				)}
+				{computerSelectedElement && (
+					<Element
+						type={computerSelectedElement}
+						className="element-computer"
+						isSelected={true}
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
